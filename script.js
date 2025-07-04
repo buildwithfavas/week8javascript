6b73f8855f8c4da7fb728be0c50c63ff7aa43d75
Favas <72879643+buildwithfavas@users.noreply.github.com>
2025-07-04 17:21:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+//1
+// const arr = [1,2,3,4,5];
+// const output = arr.map(num => num*num);
+// console.log(output);    
+
+//2
+// const arr = [12, 22, 32, 42, 52];
+// const output = arr.reduce((acc,curr) => acc += curr,0);
+// console.log(output);
+
+//3  
+// const arr = [2, 3, 5, 7, 9, 11, 13, 15, 17, 21, 25];
+// const output = arr.filter(x => x % 5 == 0);
+// console.log(output);
+
+//4
+// function* fibonacci(n) {
+//   let a = 0, b = 1;
+//   for (let i = 0; i < n; i++) {
+//     yield a;
+//     [a, b] = [b, a + b];
+//   }
+// }
+
+// const n = 5;
+// console.log(Array.from(fibonacci(n)));
+
+//5
+
