9ee61762c2b8d58b206d593f5c2e9c64b271cfa9
Favas <72879643+buildwithfavas@users.noreply.github.com>
2025-07-11 01:00:06 +0530

    Week8 assignment completed

--- script.js
+++ script.js
@@ -136,10 +136,165 @@
 // console.log(validateUserData(input4));
 
 //11
+// function average(...numbers) {
+//   if (numbers.length === 0) {
+//     return 0;
+//   }
+//   const sum = numbers.reduce((acc, curr) => acc + curr, 0);
+//   return sum / numbers.length;
+// }
+// console.log(average(10, 20, 30, 40, 50));
 
+//12
+// function checkValue(value) {
+//   // short-circuit
+//   value && console.log("Value exists");
+//   value || console.log("Value is undefined");
+// }
 
+// checkValue("Hello");       // Output: Value exists
+// checkValue(undefined);     // Output: Value is undefined
+// checkValue("");            // Output: Value is undefined
+// checkValue(0);             // Output: Value is undefined
+// checkValue("World");       // Output: Value exists
 
+//13
+// class BankAccount {
+//   #balance;
 
+//   constructor(initialBalance = 0) {
+//     this.#balance = initialBalance;
+//   }
 
+//   deposit(amount) {
+//     this.#balance += amount;
+//   }
 
+//   getBalance() {
+//     return this.#balance;
+//   }
+// }
 
+// class SavingsAccount extends BankAccount {
+//   #interestRate;
+
+//   constructor(initialBalance = 0, interestRate = 0) {
+//     super(initialBalance);
+//     this.#interestRate = interestRate;
+//   }
+
+//   addInterest() {
+//     const interest = this.getBalance() * (this.#interestRate / 100);
+//     this.deposit(interest);
+//   }
+// }
+
+// const myAccount = new SavingsAccount(0, 5);
+// myAccount.deposit(100);
+// myAccount.addInterest();
+
+// console.log("Balance:", myAccount.getBalance());
+
+//14
+// function filterEven(numbers) {
+//   return numbers.filter(num => num % 2 === 0 ? true : false);
+// }
+
+// console.log(filterEven([1, 2, 3, 4, 5, 6]));
+
+//15
+// function* generateEvenNumbers(limit) {
+//   for (let i = 2; i <= limit; i += 2) {
+//     yield i;
+//   }
+// }
+
+// const evenNumbers = generateEvenNumbers(10);
+// for (let num of evenNumbers) {
+//   console.log(num);
+// }
+
+//16
+// function transformNames(arr) {
+//   return arr.map(obj => obj.name.toUpperCase());
+// }
+
+// const input = [{ name: "Alice" }, { name: "Bob" }];
+// const output = transformNames(input);
+
+// console.log(output);
+
+//17
+// async function fetchPostTitles() {
+//   try {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! Status: ${response.status}`);
+//     }
+
+//     const posts = await response.json();
+    
+//     posts.forEach(post => {
+//       console.log(post.title);
+//     });
+//   } catch (error) {
+//     console.error("Failed to fetch posts:", error);
+//   }
+// }
+
+// fetchPostTitles();
+
+//18
+// function createDynamicObject(key, value) {
+//   return {
+//     [key]: value
+//   };
+// }
+
+// const result = createDynamicObject("color", "blue");
+// console.log(result);
+
+//19
+// function findProduct(numbers) {
+//   return numbers.reduce((acc, curr) => acc * curr, 1);
+// }
+
+// const result = findProduct([2, 3, 4, 5]);
+// console.log(result);
+
+//20
+// class Shape {
+//   area() {
+//     throw new Error("area() must be implemented by subclasses");
+//   }
+// }
+
+// class Circle extends Shape {
+//   constructor(radius) {
+//     super();
+//     this.radius = radius;
+//   }
+
+//   area() {
+//     return Math.PI * this.radius * this.radius;
+//   }
+// }
+
+// class Rectangle extends Shape {
+//   constructor(width, height) {
+//     super();
+//     this.width = width;
+//     this.height = height;
+//   }
+
+//   area() {
+//     return this.width * this.height;
+//   }
+// }
+
+// const myCircle = new Circle(5);
+// const myRectangle = new Rectangle(4, 6);
+
+// console.log("Circle area:", myCircle.area().toFixed(2));
+// console.log("Rectangle area:", myRectangle.area());
